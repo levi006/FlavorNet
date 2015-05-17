97dcf29785fc4048fa8af018f29b9574a46c4e37
levi006 <le.vi006@gmail.com>
2015-05-16 18:11:05 -0700

    Created server.py with pages listing all ingredients and flavor compounds. Working on detail pages.

---
+++ server.py
@@ -0,0 +1,42 @@
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template
+
+from model import connect_to_db, db, Ingredient, FlavorCompound
+
+
+app = Flask(__name__)
+
+# Required to use Flask sessions and the debug toolbar
+app.secret_key = "ABC"
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+
+    return render_template("homepage.html")
+
+@app.route("/ingredients")
+def ingredient_list():
+    """Show list of ingredients."""
+
+    ingredients = Ingredient.query.order_by('name').all()
+    return render_template("ingredient_list.html", ingredients=ingredients)
+
+@app.route("/flavorcompounds")
+def flavorcompounds_list():
+    """Show list of flavorcompounds."""
+
+    flavorcompounds = FlavorCompound.query.order_by('name').all()
+    return render_template("flavorcompound_list.html", flavorcompounds=flavorcompounds)
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the point
+    # that we invoke the DebugToolbarExtension
+    app.debug = True
+
+    connect_to_db(app)
+
+
+    app.run()
